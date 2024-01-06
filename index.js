9350e3f5a3727d3de218d5ada2fd69b7eccff065
rattuslover <155846208+rattuslover@users.noreply.github.com>
2024-01-06 11:35:13 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// first time using js
+let rats = 10;
+while (rats < 21) {
+    console.log("hi, i have", rats, "rats");
+    rats ++;
+}
+if (rats = 20) {
+    console.log("I have so much rats!");
+}
